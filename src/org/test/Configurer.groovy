5fb65d132340d8bb0230cf1f6f07b93e50d69826
andreas abdi <andreas.abdi@outlook.com>
2019-01-22 21:14:49 -0800

    modified configurer with properties invocation

--- src/org/test/Configurer.groovy
+++ src/org/test/Configurer.groovy
@@ -3,6 +3,11 @@ package org.test
 
 class Configurer {
     public static configure() {
-
+        def triggerFrequency = "H 4 * * *"
+        def propertiesList = []
+        def pipelineTriggersList = []
+        pipelineTriggersList.add(cron(triggerFrequency))
+        propertiesList.add(pipelineTriggers(pipelineTriggersList))
+        properties(propertiesList)
     }
 }
