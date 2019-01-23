3122be500ba353452f0278fc499848930a97155f
andreas abdi <andreas.abdi@outlook.com>
2019-01-22 21:21:42 -0800

    Made a configurable object

--- src/org/test/Configurer.groovy
+++ src/org/test/Configurer.groovy
@@ -1,13 +1,25 @@
 package org.test
 
 
-class Configurer {
+class Configurer implements Serializable {
+
+    private CpsScript script
+
+    public Configurer(CpsScript script) {
+        this.script = script
+    }
+
     public static configure() {
-        def triggerFrequency = "H 4 * * *"
-        def propertiesList = []
-        def pipelineTriggersList = []
-        pipelineTriggersList.add(cron(triggerFrequency))
-        propertiesList.add(pipelineTriggers(pipelineTriggersList))
-        properties(propertiesList)
+        invokable = {
+            def triggerFrequency = "H 4 * * *"
+            def propertiesList = []
+            def pipelineTriggersList = []
+            pipelineTriggersList.add(cron(triggerFrequency))
+            propertiesList.add(pipelineTriggers(pipelineTriggersList))
+            properties(propertiesList)
+        }
+        invokable.delegate = script
+        invokable.resolveStrategy = Closure.DELEGATE_FIRST
+        invokable()
     }
 }
